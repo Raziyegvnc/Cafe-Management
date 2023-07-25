7540bb69e21b1e9f61dfce1003141760d6c3f56b
Raziyegvnc <gvncraziye@outlook.com>
2023-07-25 09:57:26 +0300

    Project

--- src/Menu.java
+++ src/Menu.java
@@ -6,7 +6,7 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class Menu {
-    JFrame menuFrame;
+    JFrame menuFrame;//R
     JLabel menuBaseLabel;
     ImageIcon resim;
     JPanel panel;
